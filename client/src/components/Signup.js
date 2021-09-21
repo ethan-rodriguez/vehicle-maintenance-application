44b34a89d4dc32112576435f6669d8df207fd00f
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-09-21 19:47:04 -0400

    added onChange event listners and setState function

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+    
+    const [state, setState] = useState({})
+
+    const onChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value})
+    }
+
     return (
         <div>
             <form>
                 <p>Username</p>
-                    <input type='text'></input>
+                    <input onChange={onChange} name='username' type='text'></input>
                 <p>Password</p>
-                    <input type='password'></input>
+                    <input onChange={onChange} name='password' type='password'></input>
                 <p> Password Confirmation</p>
-                    <input type='password'></input><br />
+                    <input onChange={onChange} name='password_confirmation' type='password'></input><br />
                     <input type='submit'></input>
             </form>
         </div>
