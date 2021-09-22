4dafd3fd532ff5be534de80b84cccd9e47bd77c1
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-09-21 20:11:15 -0400

    set up fetch for signup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 
 function App() {
+
+  
   return (
     <div className="App">
       <Signup />
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,16 +2,32 @@ import React, { useState } from 'react'
 
 const Signup = () => {
 
-    
     const [state, setState] = useState({})
 
     const onChange = (e) => {
         setState({ ...state, [e.target.name]: e.target.value})
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const config = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(state)
+        }
+        fetch('/users', config)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit} >
                 <p>Username</p>
                     <input onChange={onChange} name='username' type='text'></input>
                 <p>Password</p>
