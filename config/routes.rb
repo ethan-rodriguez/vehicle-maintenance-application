c3773fb0a957dcd3c81ea8d927a8f91ca47060d8
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-09-23 18:25:12 -0400

    added login and logout actions to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   resources :routines
   resources :vehicles
   resources :users
+
+  # ----User Login ---
+  post '/login', to: 'sessions#create' 
+
+  post '/destroy', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
