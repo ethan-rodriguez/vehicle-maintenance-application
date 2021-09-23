b452190f4d3fdcf50fe41a668491fd540ad34e1a
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-09-23 19:01:43 -0400

    tested sign-up and login. All working!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
-            # byebug
             session[:user_id] = user.id
             render json: user, status: :created
         else
