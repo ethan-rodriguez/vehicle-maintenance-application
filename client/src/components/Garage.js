8c365b7d95459f73cb9b138a87ffa2977c3e217b
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-10-01 20:29:15 -0400

    added background image to garage.js and styling

--- client/src/components/Garage.js
+++ client/src/components/Garage.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import background from '../images/garage.jpeg'
 
 const Garage = () => {
     return (
-        <div>
+        <div id='garage__div' style={{ 
+            backgroundImage: `url(${background})`, 
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundHeight: '100vh'
+            }} >
+
             <p>this is garage</p>
         </div>
     )
