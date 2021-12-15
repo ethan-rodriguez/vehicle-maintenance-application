e7fd93be4a09496d8d5c1024f082c752469cbaf6
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-12-15 14:50:31 -0500

    adjusted some styling. added vehicle infomation to vehicle page card

--- client/src/components/VehiclePageCard.js
+++ client/src/components/VehiclePageCard.js
@@ -3,6 +3,8 @@ import React from 'react'
 const VehiclePageCard = ({vehicle}) => {
     return (
         <div>
+            <img src='https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5db8ac181a84270007892480%2F0x0.jpg' alt="vehicle image" style={{width: '30%'}}/>
+            <p>{vehicle.image_url}</p>
             <p>{vehicle.year}</p>
             <p>{vehicle.make}</p>
             <p>{vehicle.model}</p>
